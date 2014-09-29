e511efd33a129102210867ef5120050394ded43e
James Dabbs <jamesdabbs@gmail.com>
2014-09-29 12:03:13 -0400

    Allow partial Topic objects to fetch more data

--- lib/air/topic.rb
+++ lib/air/topic.rb
@@ -1,27 +1,53 @@
 module Air
   class Topic
     def self.all
+      data = Air::Api.list
+      data.map do |result|
+        Topic.new result
+      end
     end
 
     def self.find id
       data = Air::Api.fetch id
-      # { id: 2, title: 'title' }
       Topic.new data
     end
 
+    attr_reader :id, :title
+
     def initialize data
-      @id = data['id']
-      #@data = data
+      #@data = data is also a possibility ...
+      @id          = data['id']
+      @title       = data['title']
+      @description = data['description']
+      @score       = data['score']
     end
 
-    def id
-      @id
-    end
-
-    def title
+    # Customize `pry` display
+    def inspect
+      "Air::Topic(#{id} - #{title})"
     end
 
     def description
+      fetch_data! unless @description
+      @description
     end
+
+    def score
+      fetch_data! unless @score
+      @score
+    end
+
+    def fetch_data!
+      full_data = Air::Api.fetch @id
+      @description = full_data['description']
+      @score       = full_data['score']
+    end
+
+    # Clever ...
+    # %w( id title description ).each do |name|
+    #   define_method name do
+    #     ...
+    #   end
+    # end
   end
 end
